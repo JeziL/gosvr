6d75dcce53b4e8e9af0b6a066ecd92d191b20622
Li <me@wangjinli.com>
2018-10-23 09:49:23 +0800

    Update .travis.yml and build.sh.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-go get -v github.com/gobuffalo/packr
-VERSION=$(grep -Eo '_Version = "(\d+\.){2}\d+"' gosvr.go | cut -d\" -f2)
+VERSION=$(grep -Po '_Version = "(\d+\.){2}\d+"' gosvr.go | cut -d\" -f2)
 make cleanall
 mkdir release
 declare -a arr=("darwin_386" "darwin_amd64" "dragonfly_amd64" "freebsd_386" "freebsd_amd64" "freebsd_arm" "linux_386" "linux_amd64" "linux_arm" "linux_arm64" "linux_ppc64" "linux_ppc64le" "linux_mips" "linux_mipsle" "linux_mips64" "linux_mips64le" "netbsd_386" "netbsd_amd64" "netbsd_arm" "openbsd_386" "openbsd_amd64" "openbsd_arm" "plan9_386" "plan9_amd64" "solaris_amd64" "windows_386" "windows_amd64")
